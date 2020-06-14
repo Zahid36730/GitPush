cf15dd76c9e3d425993f7e0e0ff0a2770c9658ae
Zahid36730 <zh36730@gmail.com>
2020-06-14 08:40:06 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    
+    $('.owl-carousel').owlCarousel({
+        
+        autoplay:true,
+        autoplayTimeout:3000,
+        
+        autoplayHoverPause:true,
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    })
+
+    
+  });
+
+
+
